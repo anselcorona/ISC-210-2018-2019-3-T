21d6b4211bb4231dbda193326a874bc99bbcb87f
Ansel Corona <anselcorona@gmail.com>
2019-06-19 00:15:52 -0400

    ejercicio practico 2 y comienzo del 3

---
+++ PirataRPG/PirataRPG/Assets/Scripts/PiraEssence/Instantiatior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiatior : MonoBehaviour
+{
+    public List<GameObject> Essences;
+    public GameObject Spike;
+    const int _essenceQuantity = 6;
+    float _nextTime;
+    const float _LOWERTIME = 1f, _UPPERTIME = 2F;
+    void Start()
+    {
+        StartCoroutine(InstatiatorCoroutine);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator InstatiatorCoroutine()
+    {
+
+    }
+}
