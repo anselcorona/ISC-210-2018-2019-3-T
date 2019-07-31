ef52bb49de63de0b5fee227fb0c1b943b6818ed4
Ansel Corona <anselcorona@gmail.com>
2019-07-31 00:25:51 -0400

    Arreglos de ejercicio en clase 2-3

---
+++ PirataRPG/PirataRPG/Assets/Scripts/Entities/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Game
+{
+
+    public string PlayerName { get; set; }
+    public Level CurrentLevel { get; set; }
+    private static Game _instance;
+
+
+
+    public Game Instance()
+    {
+        if(_instance == null)
+        {
+            _instance = new Game();
+        }
+        return _instance;
+    }
+}
---
+++ PirataRPG/PirataRPG/Assets/Scripts/Entities/Level.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Entities
+{
+    public class Level
+    {
+        public List<List<char>> Map { get; set; }
+        public List<Character> characters { get; set; }
+        public List<Item> Items { get; set; }
+        public List<Mission> Missions { get; set; }
+    }
+}
